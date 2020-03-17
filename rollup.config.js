16c0236b94d9a84c285f0207dcec85bd092e0034
stropho <unknown@unknown.io>
2020-03-17 15:42:40 +0100

    build

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import pkg from './package.json'
+
+export default [
+
+  {
+    input: 'src/index.js',
+    external: ['fs', 'path', ...Object.keys(pkg.dependencies || {})],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'esm' },
+    ],
+    plugins: [
+    ],
+  },
+]

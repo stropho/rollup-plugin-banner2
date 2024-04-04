04468e5bf2ca44e564ec00aa09765b2c899c2bc8
stropho <3704482+stropho@users.noreply.github.com>
2024-04-04 22:51:14 +0200

    test-cli with formatter option

--- test/cli/add-one-line-banner-plugin.cjs.js
+++
@@ -1,3 +0,0 @@
-const banner2 = require('../../dist/rollup-plugin-banner2.cjs')
-
-module.exports = () => banner2(() => '// banner\n')
---
+++ test/cli/rollup.config.js
@@ -0,0 +1,14 @@
+// const banner2 = require('../../dist/rollup-plugin-banner2.es')
+import banner2 from '../../dist/rollup-plugin-banner2.es'
+
+const input = 'test/fixture/input-multi-first.js'
+export default [
+  {
+    input,
+    plugins: [banner2(() => '// banner\n')],
+  },
+  {
+    input,
+    plugins: [banner2(() => 'banner', { formatter: 'wrapAsJsdoc' })],
+  },
+]
